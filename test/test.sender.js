582ee2f39a92a3dbdac8843840064fb45884e091
yssk22 <yssk22@gmail.com>
2012-02-18 09:53:52 +0900

    test suite was rewriten based on mocha.
    - fix the order of emit callbacks.
    - FluentSender#end() now receives callback on the last argument.
    - optimized async calls.

--- test/test.sender.js
+++ test/test.sender.js
@@ -1,91 +1,84 @@
-var assert = require('assert');
+var expect = require('chai').expect;
 var sender = require('../lib/sender');
+var fluentd = require('../lib/testHelper').fluentd;
 
-module.exports = {
-  "test send object": function(){
-    var s = new sender.FluentSender('debug');
-    s.end('test send object', {
-      string: 'a',
-      number: 1,
-      object: {
-        a: 'b'
-      },
-      array: [1,2,3],
-      bool: true,
-      null: null,
-      undefined: undefined
-    }, function(err){
-      assert.isNull(err);
-    });
-  },
-
-  "test once": function(){
-    var s = new sender.FluentSender('debug');
-    s.end('test once', 'test once', function(){
-      assert.eql(s._queueTail.length, 0);
-    });
-    assert.eql(s._queue.length, 1);
-  },
-
-  "test many": function(){
-    var s = new sender.FluentSender('debug');
-    s.emit('test many', 'test many1', function(){
-      s.emit('test many', 'test many2', function(){
-        s.end();
+describe("sender", function(){
+  describe("FluentSernder", function(){
+    before(function(done){
+      fluentd(function(err, fluentd){
+        done();
       });
     });
-  },
 
-  "test many async": function(){
-    var s = new sender.FluentSender('debug');
-    var order = -1;
-    s.emit('test many async', 'test many' + (++order), function(){
-      assert.eql(order, 2);
-      order--;
-    });
-
-    s.emit('test many async', 'test many' + (++order), function(){
-      assert.eql(order, 1);
-      order--;
-    });;
-
-    s.emit('test many async', 'test many' + (++order), function(){
-      assert.eql(order, 0);
-      order--;
-      s.end();
-    });;
-    assert.eql(s._queue.length, 3);
-  },
-
-
-  "test connection error": function(){
-    var s = new sender.FluentSender('debug', {
-      host: 'localhost',
-      port: 65535
-    });
-    s.on('error', function(err){
-      assert.eql(err.code, 'ECONNREFUSED');
-      assert.eql(s._queue.length, 1);
-    });
-    s.emit('test connection error', 'foobar');
-    assert.eql(s._queue.length, 1);
-  },
-
-  "test queueing": function(){
-    var s = new sender.FluentSender('queuing', {
-      host: 'localhost',
-      port: 65535
-    });
-    s.on('error', function(err){
-      assert.eql(err.code, 'ECONNREFUSED');
-      assert.eql(s._queue.length, 1);
-      s.port = 24224;
-      s.end('test queueing', 'done', function(){
-        assert.eql(s._queue.length, 0);
+    it('should send an object and close connection.', function(done){
+      var s = new sender.FluentSender('debug');
+      var record = {
+        string: 'a',
+        number: 1,
+        object: {
+          a: 'b'
+        },
+        array: [1,2,3],
+        bool: true,
+        null: null,
+        undefined: undefined
+      };
+      s.end('test send object', record, function(){
+        // FIXME make sure the connection is closed.
+        done();
       });
-      assert.eql(s._queue.length, 2);
-    });
-    s.emit('test queueing', 'pended');
-  }
+    }); // should send an object;
 
-};
+    it('should use a queue and flush it after it ends.', function(done){
+      var s = new sender.FluentSender('debug');
+      var called = 0;
+      s.emit('1st record', '1st record', function(){
+        called++;
+        expect(called).to.be.equal(1); // confirm callbacks are called.
+      });
+      s.emit('2nd record', '2nd record', function(){
+        called++;
+        expect(called).to.be.equal(2); // confirm callbacks are called.
+      });
+      s.end('last record', 'last record', function(){
+        called++;
+        expect(called).to.be.equal(3); // confirm callbacks are called.
+        expect(s._sendQueue.length).to.be.equal(0);
+        done();
+      });
+      expect(s._sendQueue.length).to.be.equal(3);
+    });
+
+    it('should raise error when connection fails', function(done){
+      var s = new sender.FluentSender('debug', {
+        host: 'localhost',
+        port: 65535
+      });
+      s.on('error', function(err){
+        expect(err.code).to.be.equal('ECONNREFUSED');
+        done();
+      });
+      s.emit('test connection error', 'foobar');
+    });
+
+    it('should resume the connection automatically and flush the queue', function(done){
+      var s = new sender.FluentSender('queuing', {
+        host: 'localhost',
+        port: 65535
+      });
+      s.on('error', function(err){
+        expect(err.code).to.be.equal('ECONNREFUSED');
+        expect(s._sendQueue.length, 1); // buffered queue
+        // set the correct port.
+        s.port = 24224;
+        s.end('test queueing', 'done', function(){
+          expect(s._sendQueue.length).to.be.equal(0);
+          done();
+        });
+        expect(s._sendQueue.length).to.be.equal(2);
+      });
+      s.emit('test queueing', 'pended');
+      expect(s._sendQueue.length).to.be.equal(1);
+    });
+  });
+});
