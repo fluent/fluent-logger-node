b5984c830f7cf1cdf36b83e92e058f75b4cdd755
Kenji Okimoto <okimoto@clear-code.com>
2015-12-14 13:25:52 +0900

    Test for appender

--- test/test.log4js.js
+++ test/test.log4js.js
@@ -77,11 +77,9 @@ describe("log4js", function(){
 
     it('should set max listeners', function(done) {
       var appender = log4jsSupport.appender('debug');
-      log4js.addAppender(appender);
-      var logger = log4js.getLogger('mycategory');
-      expect(logger.getMaxListeners()).to.be.equal(10);
-      logger.setMaxListeners(100);
-      expect(logger.getMaxListeners()).to.be.equal(100);
+      expect(appender.getMaxListeners()).to.be.equal(10);
+      appender.setMaxListeners(100);
+      expect(appender.getMaxListeners()).to.be.equal(100);
       done();
     });
   });
